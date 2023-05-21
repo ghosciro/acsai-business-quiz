1fe91a6283337d9db3088edb7c5876553cacef56
ghosciro <dennislocatelli2001@gmail.com>
2023-05-21 22:34:33 +0200

    solved some bugs and added scrollbar

--- WINDOWS_RUN.bat
+++ WINDOWS_RUN.bat
@@ -1,4 +1,4 @@
 
 @echo off
-python  quiz.py
+python3  quiz.py
 pause
--- quiz.py
+++ quiz.py
@@ -1,7 +1,10 @@
-import pandas as pd 
 import random
+import  os
 from tkinter import *
 from tkinter import ttk
+import pandas as pd
+
+
 
 class QUIZ:
     def __init__(self, path : str,question_num:int,answer_num:int)-> object:
@@ -11,49 +14,84 @@ class QUIZ:
         self.Quiz_window = None
         self.Quiz_labels = {}
         self.Quiz_checkboxes = {}
-        self.Quiz_buttons = {}
-        self.totalwrong=0
+        self.Quiz_buttons ={}
+        self.totalwrong = 0
+        print(question_num)
         self.setup(question_num)
         self.select_num(answer_num)
         self.setup_window()
+
+        self.Quiz_canvas = Canvas(self.Quiz_window)
+        self.Quiz_scrollbar = Scrollbar(self.Quiz_window, orient="vertical", command=self.Quiz_canvas.yview)
+        self.Quiz_canvas.configure(yscrollcommand=self.Quiz_scrollbar.set)
+        self.Quiz_scrollbar.pack(side="right", fill="y")
+        self.Quiz_canvas.pack(side="left", fill="both", expand=True)
         self.create_widgets()
+
+
+        self.Quiz_canvas.create_window((0, 0), window=self.Quiz_frame, anchor="nw")
+
+        self.Quiz_frame.bind("<Configure>", self.on_frame_configure)
+        self.Quiz_frame.bind("<Enter>", self.bind_mousewheel)
+        self.Quiz_frame.bind("<Leave>", self.unbind_mousewheel)
         self.Quiz_window.mainloop()
+
+
+    def on_frame_configure(self, event):
+        self.Quiz_canvas.configure(scrollregion=self.Quiz_canvas.bbox("all"))
+
+
+    def bind_mousewheel(self, event):
+        self.Quiz_frame.bind_all("<MouseWheel>", self.on_mousewheel)
+
+    def unbind_mousewheel(self, event):
+        self.Quiz_frame.unbind_all("<MouseWheel>")
+
+    def on_mousewheel(self, event):
+        self.Quiz_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
+
     def get_questions(self):
         return self.questions.keys()    
     def setup_window(self):
         self.Quiz_window = Tk()
         self.Quiz_window.title(f"Business QUIZ - correct answers: 0/{self.correct_answers}")
-        self.Quiz_window.resizable(False, False)
+
+        self.Quiz_window.geometry("800x600")
+        self.Quiz_window.resizable(True, True)
         self.Quiz_window.config(bg="black")
     def create_widgets(self):
-        for question in self.quiz.keys():
-            self.Quiz_labels[question] = Label(self.Quiz_window, text=question, font=("Helvetica", 20),fg="white",bg="black")
-
+        self.Quiz_frame = Frame(self.Quiz_canvas, bg="black")
+        for i,question in enumerate(self.quiz.keys()):
+            self.Quiz_labels[question] = Label(self.Quiz_frame, text=question, font=("Helvetica", 20), fg="white",bg="black")
             self.Quiz_labels[question].pack(anchor=W)
-            for i,answer in enumerate(self.quiz[question]["all"]):
+            for i, answer in enumerate(self.quiz[question]["all"]):
                 if question not in self.Quiz_checkboxes.keys():
-                    self.Quiz_checkboxes[question] = [[answer,BooleanVar()]]
+                    self.Quiz_checkboxes[question] = [[answer, BooleanVar()]]
                 else:
-                    self.Quiz_checkboxes[question].append([answer,BooleanVar()])
-                    #checkbox with backround black and white text but black checkmark
-                
-                checkbox = Checkbutton(self.Quiz_window, text=answer,highlightthickness=0 ,variable=self.Quiz_checkboxes[question][i][1],bg="black",fg="white",selectcolor="black",activebackground="black",activeforeground="white",font=("Helvetica", 15))
+                    self.Quiz_checkboxes[question].append([answer, BooleanVar()])
+
+                checkbox = Checkbutton(self.Quiz_frame, text=answer, highlightthickness=0,
+                                       variable=self.Quiz_checkboxes[question][i][1], bg="black", fg="white",
+                                       selectcolor="black", activebackground="black", activeforeground="white",
+                                       font=("Helvetica", 15))
                 checkbox.pack(anchor=W)
                 self.Quiz_checkboxes[question][i].append(checkbox)
-        self.Quiz_buttons["Submit"] = Button(self.Quiz_window, text="Submit", command=self.submit)
+        self.Quiz_buttons["Submit"] = Button(self.Quiz_frame, text="Submit", command=self.submit)
         self.Quiz_buttons["Submit"].pack()
+
     def setup(self,question_num:int ):
         # read csv file
         df = pd.read_csv(self.path)
         # select random questions
-        df = df.sample(n=min(question_num,len(df)))
+        df.sample(n=min(question_num,len(df)))
         # create dictionary
-
-        for i in range(0, len(df)):
+        for i in range(0, min(question_num,len(df))):
             self.quiz[df["Question"][i]] = [
                 df["Correct Answers"][i].split(sep=","),
                 df["Wrong Answers"][i].split(sep=","),
             ]
+        print(df)
     
     def submit(self):
         correct=0
@@ -128,4 +166,7 @@ def main(path :str):
     button = Button(root, text="OK", command=lambda: [root.destroy(),QUIZ(path,int(num_questions.get()),int(num.get()))])
     button.pack()
     root.mainloop()
-__name__ == "__main__"and main("acsai-business-quiz/question.csv")
+
+
+if __name__ == "__main__":
+    main("question.csv")
